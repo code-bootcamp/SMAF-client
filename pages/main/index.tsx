71a460539b3b5eb61db8ddd70720fb4b216dcb11
owlsuri <imsimple2563@gmail.com>
2022-05-12 21:52:37 +0900

    #16-문의리스트

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,10 +1,13 @@
 import Main from "../../src/components/units/mainPage/afterLogin/main.container";
 import BeforeLogin from '../../src/components/units/mainPage/beforeLogin/beforeLogin.container'
+import QuestionAnswerList from "../../src/components/units/QuestionAnswer/list/QuestionAnswerList.container";
 
 export default function MainPage(){
 
     return(
         // <Main/>
-        <BeforeLogin />
+        // <BeforeLogin />
+        <QuestionAnswerList />
+
     )
 }
---
+++ src/components/units/QuestionAnswer/list/QuestionAnswerList.styles.ts
@@ -0,0 +1,79 @@
+import styled from "@emotion/styled";
+
+export const Wrapper=styled.div`
+margin: 0px;
+box-sizing: border-box;
+background-color: #E5E5E5;
+padding: 100px;
+`
+export const Container=styled.div`
+margin: auto;
+border-radius: 30px;
+`
+export const Title=styled.div`
+text-align: center;
+padding-bottom: 30px;
+font-size: 32px;
+font-weight: 700;
+`
+export const TableHeader=styled.div`
+font-size: 16px;
+font-weight: 600;
+background-color: #E5E5EF;
+border-top-left-radius: 8px;
+border-top-right-radius: 8px;
+font-size: 16px;
+display: flex;
+padding: 8px 20px 8px 25px;
+`
+export const Table=styled.tr`
+font-size: 16px;
+display: flex;
+padding: 8px 20px 8px 25px;
+border-bottom: 1px solid #C4C4C4;
+background-color: white;
+`
+export const TdNum=styled.div`
+width: 7%;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+export const TdType=styled.div`
+width: 15%;
+display: flex;
+align-items: center;
+justify-content: center;
+`
+export const TdTitle=styled.div`
+width: 43%;
+padding-left: 20px;
+`
+export const TdWriter=styled.div`
+width: 10%;
+`
+export const TdDate=styled.div`
+width: 12%;
+text-align: center;
+`
+export const AnswerBtn=styled.button`
+width: 13%;
+background-color: #6B6BFF;
+color: white;
+width: 79.92px;
+height: 31px;
+font-size: 16px;
+font-weight:500;
+border: none;
+border-radius: 8px;
+`
+export const AnswerCompleteBtn=styled.button`
+background-color: #49496E;
+color: white;
+width: 79.92px;
+height: 31px;
+font-size: 16px;
+font-weight:500;
+border: none;
+border-radius: 8px;
+`
