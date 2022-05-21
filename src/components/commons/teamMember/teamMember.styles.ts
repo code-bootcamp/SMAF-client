837426c047de98f4eca53d9843608b15a265ae34
gkemzm <gkemzm@gmail.com>
2022-05-21 17:40:27 +0900

    feature-#90

--- src/components/commons/teamMember/teamMember.styles.ts
+++ src/components/commons/teamMember/teamMember.styles.ts
@@ -1,65 +1,63 @@
 import styled from "@emotion/styled";
 
-export const Wrapper=styled.div`
-width: 33rem;
-background-color: white;
-border-radius: 1.6rem;
-padding: 1.5rem;
-`
-export const Title=styled.div`
-font-size: 2.4rem;
-font-weight: 500;
-`
-export const TitleBox=styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding-bottom: 2rem;
-`
-export const TeamPeople=styled.div`
-
-`
-export const TeamPerson=styled.img`
-width: 4.8rem;
-height: 4.8rem;
-border-radius: 3rem;
-`
-export const Name=styled.div`
-padding-left: 1rem;
-font-weight: 500;
-font-size: 1.6rem;
-`
-export const Team=styled.div`
-font-size: 1.6rem;
-display: flex;
-align-items: center;
-justify-content: space-between;
-padding: 0.7rem 0 0.7rem 0;
-`
-export const Line=styled.div`
-border-bottom: 0.1rem solid #EDEDED;
-`
-export const TeamPersonAdd=styled.div`
-width: 4.8rem;
-height: 4.8rem;
-border-radius: 3rem;
-background-color: #333333;
-color: white;
-font-size: 3rem;
-display: flex;
-align-items: center;
-justify-content: center;
-padding-bottom: 0.7rem;
-`
-export const Member=styled.div`
-display: flex;
-align-items: center;
-`
-export const RemoveBtn=styled.button`
-width: 4.6rem;
-height: 2.6rem;
-font-size: 1.2rem;
-background-color: #EDEDED;
-border-radius: 1.3rem;
-border: none;
-`
+export const Wrapper = styled.div`
+  width: 22rem;
+  background-color: white;
+  border-radius: 1.6rem;
+  padding: 1.5rem;
+`;
+export const Title = styled.div`
+  font-size: 2.4rem;
+  font-weight: 500;
+`;
+export const TitleBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 2rem;
+`;
+export const TeamPeople = styled.div``;
+export const TeamPerson = styled.img`
+  width: 4.8rem;
+  height: 4.8rem;
+  border-radius: 3rem;
+`;
+export const Name = styled.div`
+  padding-left: 1rem;
+  font-weight: 500;
+  font-size: 1.6rem;
+`;
+export const Team = styled.div`
+  font-size: 1.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.7rem 0 0.7rem 0;
+`;
+export const Line = styled.div`
+  border-bottom: 0.1rem solid #ededed;
+`;
+export const TeamPersonAdd = styled.div`
+  width: 4.8rem;
+  height: 4.8rem;
+  border-radius: 3rem;
+  background-color: #333333;
+  color: white;
+  font-size: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 0.7rem;
+`;
+export const Member = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const RemoveBtn = styled.button`
+  width: 4.6rem;
+  height: 2.6rem;
+  font-size: 1.2rem;
+  background-color: #ededed;
+  border-radius: 1.3rem;
+  border: none;
+`;
---
+++ src/components/units/detailPage/detailPage.querys.ts
@@ -0,0 +1,95 @@
+import { gql } from "@apollo/client";
+// 카테고리 생성
+export const CREATE_PROCESS_CATEGORY = gql`
+  mutation createProcessCategory($processName: String!, $projectId: String!) {
+    createProcessCategory(processName: $processName, projectId: $projectId) {
+      processCategoryId
+    }
+  }
+`;
+// 프로젝트 조회
+export const FETCH_PROJECT = gql`
+  query fetchProject($projectId: String!) {
+    fetchProject(projectId: $projectId) {
+      projectId
+      projectName
+      projectIntro
+      projectDetailIntro
+      projectImageURL
+      startDate
+      endDate
+      status
+      projectColor
+      createAt
+      address {
+        projectAddressId
+        address
+        detailAddress
+      }
+    }
+  }
+`;
+// 카테고리 조회
+export const FETCH_PROCESS_CATEGORIES = gql`
+  query fetchProcessCategories($projectId: String!) {
+    fetchProcessCategories(projectId: $projectId) {
+      processCategoryId
+      processName
+      createAt
+    }
+  }
+`;
+
+// 프로젝트 ID로 전체 스케줄 조회
+export const FETCH_PROJECT_SCHEDULES_PROJECTID = gql`
+  query fetchProjectSchedules($projectId: String!) {
+    fetchProjectSchedules(projectId: $projectId) {
+      scheduleId
+      scheduleName
+      scheduleDate
+      status
+      processCategory {
+        processCategoryId
+      }
+      project {
+        projectId
+      }
+      createAt
+    }
+  }
+`;
+
+// 카테고리 ID로 전체 스케줄 조회
+// export const FETCH_PROJECT_SCHEDULES_CATEGORY = gql`
+//   query fetchCategorySchedules($processCategoryId: String!) {
+//     fetchCategorySchedules(processCategoryId: $processCategoryId) {
+//       scheduleId
+//       scheduleName
+//       scheduleDate
+//       status
+//       processCategory {
+//         processCategoryId
+//       }
+//       project {
+//         projectId
+//       }
+//       createAt
+//     }
+//   }
+// `;
+
+// 스케쥴 삭제
+export const DELETE_SCHEDULE = gql`
+  mutation deleteSchedule($scheduleId: String!) {
+    deleteSchedule(scheduleId: $scheduleId)
+  }
+`;
+
+// 스케쥴 생성
+export const CREATE_SCHEDULE = gql`
+  mutation createSchedule($createScheduleInput: CreateScheduleInput!) {
+    createSchedule(createScheduleInput: $createScheduleInput) {
+      scheduleId
+    }
+  }
+`;
